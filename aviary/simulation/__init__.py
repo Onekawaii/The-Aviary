5bb344f41f76c3e45097f57c0eaa3858f7c35b5a
James Gargano <jmgar81@gmail.com>
2026-08-02 15:25:16 -0400

    feat: expose simulation core

---
+++ aviary/simulation/__init__.py
@@ -0,0 +1,17 @@
+from aviary.simulation.contracts import (
+    EntityBlueprint,
+    ReplayResult,
+    SimulationEvent,
+    SimulationSnapshot,
+    SimulationValidationError,
+)
+from aviary.simulation.kernel import DeterministicSimulation
+
+__all__ = [
+    "DeterministicSimulation",
+    "EntityBlueprint",
+    "ReplayResult",
+    "SimulationEvent",
+    "SimulationSnapshot",
+    "SimulationValidationError",
+]
