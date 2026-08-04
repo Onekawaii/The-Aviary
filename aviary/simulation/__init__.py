03c3291e07b312be0b5ae46898cfa1b952999382
James Gargano <jmgar81@gmail.com>
2026-08-04 13:16:10 -0400

    fix: reconcile simulation exports with chronicle core

--- aviary/simulation/__init__.py
+++ aviary/simulation/__init__.py
@@ -1,3 +1,5 @@
+from aviary.simulation.archive import Archive
+from aviary.simulation.chronicle import Chronicle
 from aviary.simulation.contracts import (
     EntityBlueprint,
     ReplayResult,
@@ -11,15 +13,19 @@ from aviary.simulation.dialogue import (
     DialogueResult,
     GovernorVerdict,
 )
+from aviary.simulation.fossil import Fossil
 from aviary.simulation.kernel import DeterministicSimulation
 from aviary.simulation.persistence import SimulationReceiptStore, StoredSimulation
 
 __all__ = [
+    "Archive",
+    "Chronicle",
     "DeterministicDialogue",
     "DeterministicSimulation",
     "DialogueRecord",
     "DialogueResult",
     "EntityBlueprint",
+    "Fossil",
     "GovernorVerdict",
     "ReplayResult",
     "SimulationEvent",
